7e7819d7b95e3a2f89a7a0775d862c5d13fbfb21
yusufansari563 <yusufansari563@gmail.com>
2020-11-12 11:16:48 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+//  Code By Webdevtrick ( https://webdevtrick.com ) 
+$(document).ready(function() {
+    $('#testimonials-list').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
+        }
+    });
+});
